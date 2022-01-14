6e4ed426772778091af8678304ce0645ab44dfc8
Gabriele Castro <gabrielerayane2001@gmail.com>
2022-01-13 21:24:09 -0300

    Corrige tamble

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,16 +4,18 @@ class Table extends Component {
   render() {
     return (
       <table>
-        <tr>
-          <td>Descrição</td>
-          <td>Tag</td>
-          <td>Método de pagamento</td>
-          <td>Valor</td>
-          <td>Moeda</td>
-          <td>Câmbio utilizado</td>
-          <td>Valor convertido</td>
-          <td>Moeda de conversão</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+          </tr>
+        </thead>
       </table>
     );
   }
