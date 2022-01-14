e6cea9dda5dd51c95c42e0b1e5ebb5d415d23063
Gabriele Castro <gabrielerayane2001@gmail.com>
2022-01-14 08:49:43 -0300

    Adiciona atributo role na tabela

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
+/* import { connect } from 'react-redux'; */
 
 class Table extends Component {
   render() {
+    /*  const { infos } = this.props; */
+    console.log(infos);
     return (
       <table>
         <thead>
           <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
+            <th role="columnheader">Descrição</th>
+            <th role="columnheader">Tag</th>
+            <th role="columnheader">Método de pagamento</th>
+            <th role="columnheader">Valor</th>
+            <th role="columnheader">Moeda</th>
+            <th role="columnheader">Câmbio utilizado</th>
+            <th role="columnheader">Valor convertido</th>
+            <th role="columnheader">Moeda de conversão</th>
           </tr>
         </thead>
       </table>
     );
   }
 }
+
+/* const mapStateToProps = (state) => ({
+  infos: state.wallet.exchangeRates,
+}); */
 
 export default Table;
